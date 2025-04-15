7100392e76bf77d782d393b23232d9797cf15e9f
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2025-04-15 14:32:27 +0200

    Update with content

--- utilities/lab-build.sh
+++ utilities/lab-build.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
+#
 
 echo "Starting build process..."
-
-# Remove old site files
-echo "Removing old site files..."
+echo "Removing old site..."
 rm -rf ./www/*
-echo "Old site files removed."
-
-# Build new site
 echo "Building new site..."
-npx antora --fetch default-site.yml --stacktrace
 
-# Completion message
+npx antora --fetch default-site.yml
+
 echo "Build process complete. Check the ./www folder for the generated site."
-echo "To view the site locally, run: utilities/lab-action.sh serve"
-
-
+echo "To view the site locally, run the following command: utilities/lab-serve"
+echo "If already running then browse to http://localhost:8080/index.html"
